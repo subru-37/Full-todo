04e31719274a1b573dab10cfbe07db8b9ae5b1d8
subru <easwaransubramaniOO7@gmail.com>
2023-07-01 21:18:12 +0530

    connecting to supabase db

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.json())
 app.get('/todos/:userEmail',async (req,res)=>{
     const {userEmail} = req.params;
     try{   
-        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1;',[userEmail]);
+        const todos = await supabase.query('SELECT * FROM todos WHERE user_email = $1;',[userEmail]);
         res.json(todos.rows)
     }   
 
@@ -33,7 +33,7 @@ app.post('/todos', async (req,res)=>{
     const {user_email, title, progress, date} = req.body;
     const id = uuidv4();
     try{
-        const toDo = await pool.query(`INSERT INTO todos(id,user_email, title, progress, date) VALUES($1,$2,$3,$4,$5);`,[id,user_email, title, progress, date] );
+        const toDo = await supabase.query(`INSERT INTO todos(id,user_email, title, progress, date) VALUES($1,$2,$3,$4,$5);`,[id,user_email, title, progress, date] );
         console.log(id,user_email, title, progress, date)
         res.json(toDo)
     }catch(err){
@@ -44,7 +44,7 @@ app.put('/todos/:id', async (req,res)=>{
     const {id} = req.params;
     const {user_email, title, progress, date} = req.body;
     try{
-        const editTodo = await pool.query(`UPDATE todos SET user_email = $1, title = $2, progress = $3, date=$4 WHERE id=$5;`,[user_email,title,progress,date,id])
+        const editTodo = await supabase.query(`UPDATE todos SET user_email = $1, title = $2, progress = $3, date=$4 WHERE id=$5;`,[user_email,title,progress,date,id])
         res.json(editTodo);
     }
     catch(err){
@@ -55,7 +55,7 @@ app.delete('/todos/:id', async (req,res)=>{
     const {id} = req.params;
     console.log(id)
     try{
-        const deleteTodo = await pool.query(`DELETE FROM todos WHERE id = $1;`,[id]);
+        const deleteTodo = await supabase.query(`DELETE FROM todos WHERE id = $1;`,[id]);
         res.json(deleteTodo);
     }
     catch(err){
@@ -69,7 +69,7 @@ app.post('/signup',async (req,res)=>{
     const hashpass = bcrypt.hashSync(password,salt);
     try{
         console.log(`INSERT INTO users (email,hashed_password) VALUES($1,$2)`,[email,hashpass])
-        const signup = await pool.query(`INSERT INTO users (email,hashed_password) VALUES($1,$2)`,[email,hashpass]);
+        const signup = await supabase.query(`INSERT INTO users (email,hashed_password) VALUES($1,$2)`,[email,hashpass]);
         const token = jwt.sign({email},'secret',{expiresIn:'1hr'});
         res.json({email,token})
     }catch(err){
@@ -83,7 +83,7 @@ app.post('/login',async (req,res)=>{
     const {email,password} = req.body;
 
     try{
-        const users = await pool.query('SELECT * FROM users WHERE EMAIL= $1',[email])
+        const users = await supabase.query('SELECT * FROM users WHERE EMAIL= $1',[email])
         if(!users.rows.length){
             return res.json({detail: 'User does not exist, please sign up'})
         }
